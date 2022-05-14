71d27d05b8297074aef802fc73c994f438ecdb09
SaleemiHaseebullah2001 <73466496+SaleemiHaseebullah2001@users.noreply.github.com>
2022-05-14 19:00:02 +0200

    Major Modification
    Added Favicon
    Changed Font Body Font
    Added 404.html
    Modified main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ function cf_CHECK()
     let regExp = /^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$/;
     let cf = document.getElementById('input');
     if (cf.value.search(regExp) == -1) {
-        document.getElementById("wrong").innerHTML = '<span  class=\"fas fa-window-close"\ style=\"font-size:24px;color:red"\></span>';
+        document.getElementById("result").innerHTML = '<span  class=\"fas fa-window-close"\ style=\"font-size:28px;color:red"\></span>';
     }else{
-        document.getElementById("correct").innerHTML = '<span class=\"fas fa-check-square"\  style=\"font-size:24px;color:green"\></span>';
-        document.getElementById("wrong").remove();
+        document.getElementById("result").innerHTML = '<span class=\"fas fa-check-square"\  style=\"font-size:28px;color:green"\></span>';
     }
 }
